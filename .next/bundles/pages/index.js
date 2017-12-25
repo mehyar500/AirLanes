560843577debdacbc5614d0ed09af150d42302ea
mehyar500 <mehyar500@gmail.com>
2017-12-25 01:15:23 -0500

    Next js

---
+++ .next/bundles/pages/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([5],{221:function(e,t,l){e.exports=l(222)},222:function(e,t,l){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}function s(){return[{id:"hello-nextjs",title:"Hello Next.js"},{id:"learn-nextjs",title:"Learn Next.js is awesome"},{id:"deploy-nextjs",title:"Deploy apps with ZEIT"}]}Object.defineProperty(t,"__esModule",{value:!0});var n=l(115),u=a(n),i=l(1),o=a(i),r=l(74),d=a(r),c=l(110),f=a(c);t.default=function(){return o.default.createElement(d.default,null,o.default.createElement("h1",{className:"jsx-4028648676"},"My Blog"),o.default.createElement("ul",{className:"jsx-4028648676"},s().map(function(e){return o.default.createElement("li",{key:e.id,className:"jsx-4028648676"},o.default.createElement(f.default,{as:"/p/"+e.id,href:"/post?title="+e.title},o.default.createElement("a",{className:"jsx-4028648676"},e.title)))})),o.default.createElement(u.default,{styleId:"4028648676",css:['h1.jsx-4028648676,a.jsx-4028648676{font-family:"Arial";}',"ul.jsx-4028648676{padding:0;}","li.jsx-4028648676{list-style:none;margin:5px 0;}","a.jsx-4028648676{text-decoration:none;color:blue;}","a.jsx-4028648676:hover{opacity:0.6;}"]}))}}},[221]);
+            return { page: comp.default }
+          })
+        
