2620d66531de75dac1cc239497020c4e1db5592e
mehyar500 <mehyar500@gmail.com>
2018-01-02 19:54:17 -0500

    ready webpack dev server , hot module reloading

--- server.js
+++ server.js
@@ -2,15 +2,54 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require("path");
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const dbConnection = require('./db'); // loads our connection to the mongo database
 const app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: "502e29ead6874bee9d4869b08a336317",
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
+// ===== Middleware ====
+app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(
+  session({
+    secret: process.env.APP_SECRET || "this is the default passphrase",
+    store: new MongoStore({ mongooseConnection: dbConnection }),
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
+// ==== if its production environment!
+if (process.env.NODE_ENV === 'production') {
+	console.log('YOU ARE IN THE PRODUCTION ENV')
+	app.use('/static', express.static(path.join(__dirname, '../build/static')))
+	app.get('/', (req, res) => {res.sendFile(path.join(__dirname, '../build/'))});
+}
 
+// ====== Error handler ====
+app.use(function(err, req, res, next) {
+	console.log('====== ERROR =======')
+	console.error(err.stack)
+	res.status(500)
+})
+
+// Use the rollbar error handler to send exceptions to your rollbar account
+app.use(rollbar.errorHandler());
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
